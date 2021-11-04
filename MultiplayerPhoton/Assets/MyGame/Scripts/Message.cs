556d2f45107cf9a2dd6c9d136c6c14efe1bdefb4
Thiago Garcia <thiagoncg1@gmail.com>
2021-11-04 20:07:36 -0300

    Notificação
    Criado sistema de notificação.

--- MultiplayerPhoton/Assets/MyGame/Scripts/Message.cs
+++ MultiplayerPhoton/Assets/MyGame/Scripts/Message.cs
@@ -1,8 +1,53 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class Message : MonoBehaviour
+using Photon.Pun;
+using Photon.Realtime;
+
+public class Message : MonoBehaviourPunCallbacks
 {
+    [Header("Componentes de Menssagens")]
+    public Text textMsg;
+    public float timeForHide = 10f;
 
+    [Space]
+    [Header("Menssagens na Tela")]
+    public string msgEnter = "Entrou na Sala";
+    public string msgGameOuver = "Saiu da Sala";
+
+    private void Awake()
+    {
+        textMsg.gameObject.SetActive(false);        
+    }
+
+    private void Start()
+    {
+        textMsg.text = (PhotonNetwork.NickName);
+    }
+
+    void MostrarMsg(string msg)
+    {
+        textMsg.text = msg;
+        textMsg.gameObject.SetActive(true);
+
+        StartCoroutine(WaitForHide(timeForHide));
+    }
+
+    IEnumerator WaitForHide(float waitForTime)
+    {
+        yield return new WaitForSeconds(waitForTime);
+        textMsg.gameObject.SetActive(false);
+    }
+
+    public override void OnPlayerEnteredRoom(Player newPlayer)
+    {
+        MostrarMsg(newPlayer.NickName + msgEnter); // "Nome do Player Entrou na sala".
+    }
+
+    public override void OnPlayerLeftRoom(Player otherPlayer)
+    {
+        base.OnPlayerLeftRoom(otherPlayer); // "nome Player Saiu da Sala".
+    }
 }
